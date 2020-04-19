1fcd9dd677a92a3f6a981f04300501aefd816185
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-04-19 18:49:30 -0300

    :tada: finish challenge

---
+++ README.md
@@ -0,0 +1,37 @@
+<p align="center">
+    <img alt="GoStack" src="https://i.lensdump.com/i/jCEM0c.png" />
+</p>
+
+## :rocket: Challenge 05: <span style="color:lightgreen;">NodeJS</span> Fundamentals
+
+NodeJS Fundamentals for the **second week(F 03)** of Bootcamp GoStack 11.
+
+- [About Challenge](#https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-fundamentos-nodejs)
+
+Challenge completed on 19/04/2020.
+
+## :gear: Getting Started
+
+```Bash
+# Install dependencies
+yarn install
+
+# Run application
+yarn dev:server
+
+# Run tests
+yarn test
+```
+> Access the URL in your browser http://localhost:3333
+
+## Demo
+
+<img alt="Bootcamp GoStack Challenge 05" src="https://i.lensdump.com/i/jJU60Z.gif" />
+
+## :man_astronaut: Author
+
+**Guilherme Bezerra** ©️
+
+[![github](http://ap.imagensbrasil.org/images/2018/12/10/github-logo-1.png) ](http://www.github.com/gbdsantos)
+[![gitlab](http://ap.imagensbrasil.org/images/2018/12/10/gitlab-32.png)](https://gitlab.com/gbdsantos1)
+[![linkedin](http://ap.imagensbrasil.org/images/2018/12/10/linkedin-1.png)](https://www.linkedin.com/in/gbdsantos/)
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,22 @@
 import { Router } from 'express';
+import CreateTransactionService from '../services/CreateTransactionService';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    // TODO:
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({
+      transactions,
+      balance,
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +24,28 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    /**
+     * TODO: POST /transactions:
+     * A rota deve receber title, value, type, e category dentro do
+     * corpo da requisição, sendo o type o tipo da transação, que deve ser
+     * income para entradas (depósitos) e outcome para saídas (retiradas).
+     * Ao cadastrar uma nova transação, ela deve ser armazenada dentro do seu
+     * banco de dados, possuindo os campos id, title, value, type, category_id,
+     * created_at, updated_at
+     */
+    const { title, value, type } = request.body;
+
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = createTransaction.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
